eefb3a71357f939983b18cd6e0210b5a12f9ab5d
chinmay0208 <mundhadachinmay@gmail.com>
2020-06-02 15:45:14 +0530

    New updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,94 @@
-$(document).ready(function(){
-    $("#basicForm").validate();
-})
+
+            function isEmail(email) {
+  
+                var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  
+                return regex.test(email);
+                
+            }
+
+            function CheckPassword(password) { 
+                var passw=  /^[A-Za-z]\w{7,14}$/;
+                return passw.test(password);
+            }
+
+
+
+            
+            $("#submitButton").click(function() {
+                
+                var errorMessage = "";
+                var fieldsMissing = "";
+                
+                if ($("#email").val() == "") {
+                    
+                    fieldsMissing += "<br>Email";
+                    
+                }
+                
+                if ($("#phone").val() == "") {
+                    
+                    fieldsMissing += "<br>Telephone";
+                    
+                }
+                
+                if ($("#password").val() == "") {
+                    
+                    fieldsMissing += "<br>Password";
+                    
+                }
+                
+                if ($("#passwordConfirm").val() == "") {
+                    
+                    fieldsMissing += "<br>Confirm Password";
+                    
+                }
+                
+                if (fieldsMissing != "") {
+                    
+                    errorMessage += "<p>The following field(s) are missing:" + fieldsMissing;
+                    
+                }
+                
+                if (isEmail($("#email").val()) == false) {
+                    
+                    errorMessage += "<p>Your email address is not valid</p>";
+                    
+                }
+                
+                if ($.isNumeric($("#phone").val()) == false) {
+                    
+                    errorMessage += "<p>Your phone number is not numeric</p>";
+                    
+                }
+
+                if (CheckPassword($("password").val())== false){
+                     errrorMessage += "<p>Your password does not match the required constraints</p>";
+                }
+
+                if ($("#password").val() != $("#passwordConfirm").val()) {
+                    
+                    errorMessage += "<p>Your passwords don't match</p>";
+                    
+                }
+
+                if ($("#password").val() != $("#passwordConfirm").val()) {
+                    
+                    errorMessage += "<p>Your passwords don't match</p>";
+                    
+                }
+                
+                if (errorMessage != "") {
+                    
+                    $("#errorMessage").html(errorMessage);
+                    
+                } else {
+                    
+                    $("#successMessage").show();
+                    $("#errorMessage").hide();
+                    
+                }
+                
+            });
+            
+            
